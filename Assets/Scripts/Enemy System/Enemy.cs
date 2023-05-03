c4b8b72d073823ef90f45f9ceaf6bd53ec42c80a
SeanoGallagher <36253804+SeanoGallagher@users.noreply.github.com>
2023-05-02 18:26:33 -0700

    Update Enemy.cs

--- Assets/Scripts/Enemy System/Enemy.cs
+++ Assets/Scripts/Enemy System/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,61 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public List<ShortItem> invent;
+    public float moveSpeed = 10f;
+    public float attackSpeed = 0.5f;
+    public int damage = 33;
+
+    private GameObject player;
+    private float timeSinceAttack = 0;
+    private bool attacking = false;
+    private GameObject target;
+    private Health targetHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeSinceAttack += Time.deltaTime;
+        if(!attacking)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            if(!target.activeInHierarchy)
+            {
+                attacking = false;
+            }
+            else if(timeSinceAttack >= attackSpeed)
+            {
+                Attack();
+            }
+        }
     }
+
+    private void Attack()
+    {
+        targetHealth.TakeDamage(damage);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        target = collision.gameObject;
+        targetHealth = target.GetComponent<Health>();
+        attacking = true;
+
+        Debug.Log("Attacking " + target);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        attacking = false;
+    }
+
+
 }
